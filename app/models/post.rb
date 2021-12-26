fccddd5d4e5b8aa85d69914494d9f1c0f1633ee9
MichaelPisciotta <mfp56@miami.edu>
2021-12-26 11:21:32 -0500

    didnt get custom working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }    
+    validates :category, inclusion: { in: (['Fiction', 'Non-Fiction'])}
+    # validate :click_bait
+
+    # #custom validations
+    # def click_bait
+    #     if title = 'True Facts'
+    #         errors.add(:title, "Sorry")
+    #     end
+    # end
+
 end
